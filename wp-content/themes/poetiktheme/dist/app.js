35765acc71d89a8f85b5810232f29d5c4248201b
Olga <olga_ze@inbox.lv>
2021-02-26 15:30:20 +0200

    build

--- wp-content/themes/poetiktheme/dist/app.js
+++ wp-content/themes/poetiktheme/dist/app.js
@@ -7,7 +7,64 @@
   \********************/
 /***/ (() => {
 
-console.log("this is coming from mix");
+function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+console.log("this is coming from mix"); //READ MORE LINK
+
+var readMore = document.querySelector(".about__read-more");
+var modal = document.querySelector(".modal");
+var body = document.querySelectorAll("section");
+var footer = document.querySelector("footer");
+var x = document.querySelector(".modal__close-btn");
+var heading = document.querySelector(".about__title");
+readMore.addEventListener("click", function () {
+  var _iterator = _createForOfIteratorHelper(body),
+      _step;
+
+  try {
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var section = _step.value;
+
+      if (section.className !== "modal") {
+        section.style.display = "none";
+      }
+    }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
+  }
+
+  footer.style.display = "none";
+  modal.style.visibility = "visible";
+  modal.scrollIntoView();
+});
+x.addEventListener("click", function () {
+  var _iterator2 = _createForOfIteratorHelper(body),
+      _step2;
+
+  try {
+    for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
+      var section = _step2.value;
+
+      if (section.className !== "modal") {
+        section.style.display = "block";
+      }
+    }
+  } catch (err) {
+    _iterator2.e(err);
+  } finally {
+    _iterator2.f();
+  }
+
+  footer.style.display = "block";
+  modal.style.visibility = "hidden";
+  heading.scrollIntoView();
+});
 
 /***/ }),
 
